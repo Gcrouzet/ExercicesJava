b64a652f9cea666bae9e53cdd2fbb6f92e439c78
Gcrouzet <ga.crouzet@hotmail.fr>
2020-01-09 09:38:46 +0100

    452

---
+++ ProjetTestt/src/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sylvain Torrenti
+ * @version 1.0
+ * @since 17/12/19
+ */
+public class HelloWorld {
+
+	/**
+	 * @author Dev2
+	 * @param args paramètres du programme
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nom = "Sylvain";
+		String ajout = " a réussi"; 
+		String fin = "encore une fois";
+		System.out.println("Hello World " + nom + ajout);
+		System.out.println("coucou\nC'est magique\ndernier message\n"+fin);
+	} 
+}
