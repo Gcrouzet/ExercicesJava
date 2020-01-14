f46b5a4c5b3880989eab1c13c5b11728c672872a
Gcrouzet <ga.crouzet@hotmail.fr>
2020-01-14 10:45:37 +0100

    billet train v3

--- ProjetTest/src/Exercice/RéductionBilletTrain1.java
+++ ProjetTest/src/Exercice/RéductionBilletTrain1.java
@@ -22,7 +22,7 @@ public class RéductionBilletTrain1 {
 		rectangle.close();
 
 		if (age >= 25) {
-			if (age > 60) {
+			if (age >= 60) {
 				System.out.println("Vous avez le droit à une réduction de 30%.");
 			} else {
 				System.out.println("Vous n'avez droit à aucune réduction.");
--- ProjetTest/src/Exercice/RéductionBilletTrain3.java
+++ ProjetTest/src/Exercice/RéductionBilletTrain3.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  * @author CRM
  *
  */
-public class RéductionBilletTrain2 {
+public class RéductionBilletTrain3 {
 
 	/**
 	 * @param args
@@ -21,15 +21,17 @@ public class RéductionBilletTrain2 {
 		int age = rectangle.nextInt();
 		rectangle.close();
 
-		if (age < 25) {
-			System.out.println("Vous avez le droit à une réduction de 50%.");
-		} else if (age >= 60) {
-			System.out.println("Vous avez le droit à une réduction de 30%.");
-		}
-		if (age >= 25 && age < 60) {
+		boolean age1 = age >= 25 && age <= 60 ? true : false;
+		if (age1 == true) {
 			System.out.println("Vous n'avez droit à aucune réduction.");
+		} else {
+			System.out.print("Vous avez le droit à une réduction");
 		}
-
+		if (age < 25) {
+			System.out.println(" de 50%");
+		} else if (age >= 60) {
+			System.out.println(" de 30%");
+		}
 	}
 
 }
