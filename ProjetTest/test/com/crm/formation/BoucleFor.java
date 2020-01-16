1d1ae439ae7c4fa81cde442bd4b1c2a74a652149
Gcrouzet <ga.crouzet@hotmail.fr>
2020-01-16 09:30:44 +0100

    for

--- ProjetTest/test/com/crm/formation/BoucleFor.java
+++ ProjetTest/test/com/crm/formation/BoucleFor.java
@@ -13,11 +13,20 @@ public class BoucleFor {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			System.out.println(i);
-			
-		}
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println(i);
 
+		for (int x = -5; x <= 5; x++) {
+			if (x == 0) {
+				System.out.println("division par 0");
+//				break;
+//				continue;
+				 return;
+				
+			}
+			System.out.println(1.0 / x);
+		}
+		System.out.println("ça ne marchera pas");
 	}
 
 }
