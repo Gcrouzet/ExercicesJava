592aaf5ea11f947df5d306de9dce91a54f55bf37
Gcrouzet <ga.crouzet@hotmail.fr>
2020-01-16 15:54:06 +0100

    return

---
+++ ProjetTest/test/com/crm/formation/instructionReturnTest.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+/**
+ * @author CRM
+ *
+ */
+public class instructionReturnTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int nombre=10;
+		afficherListeNombre(nombre);
+	}
+	public static void afficherListeNombre(int nb) {
+		System.out.println("Début méthode afficherListeNombre");
+		for(int i=0;i<nb;i++) {
+			if (i==5) {
+				return;
+			}
+			System.out.println(i);
+		}
+		System.out.println("Fin méthode afficherListeNombre");
+
+	}
+
+}
