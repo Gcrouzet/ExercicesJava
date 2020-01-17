4d155611a5bcfbcc583ed644948bc68c2f3edfed
Gcrouzet <ga.crouzet@hotmail.fr>
2020-01-17 09:35:38 +0100

    tableau

---
+++ ProjetTest/test/com/crm/formation/TableauTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+/**
+ * @author CRM
+ *
+ */
+public class TableauTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String tableau1[] = { "gabriel", "crouzet"};
+		System.out.println(tableau1[0]);
+
+	}
+
+}
